3b2a2e56dd084d39dabef9c97df5548bc064398a
Jordy Dickinson <jordy.dickinson@icloud.com>
2019-12-06 15:27:02 -0500

    Add day 6 part 2

--- src/day6.ml
+++ src/day6.ml
@@ -11,6 +11,31 @@ module Starmap = struct
       Map.add_exn map child parent
     in
     In_channel.fold_lines file ~init:String.Map.empty ~f:read_line
+
+  let fold_up (starmap : t) ~start ~init ~f =
+    let rec fold_up' accum node =
+      let accum = f accum node in
+      match Map.find starmap node with
+      | None -> accum
+      | Some parent ->
+        fold_up' accum parent
+    in
+    fold_up' init start
+
+  let distance starmap ~node1 ~node2 =
+    let path node =
+      fold_up starmap ~start:node ~init:[] ~f:(fun path node -> node :: path)
+    in
+    let path1 = path node1 in
+    let path2 = path node2 in
+    let prefixlen =
+      let pathzip, _ = List.zip_with_remainder path1 path2 in
+      let noprefix = List.drop_while pathzip ~f:(fun (a, b) -> [%equal: string] a b) in
+      List.length pathzip - List.length noprefix
+    in
+    let path1 = List.drop path1 prefixlen in
+    let path2 = List.drop path2 prefixlen in
+    List.length path1 + List.length path2 - 2
   
   let compute_checksum starmap =
     let memo = String.Map.empty in
@@ -42,3 +67,8 @@ let part1 file =
   let starmap = Starmap.read_exn file in
   let checksum = Starmap.compute_checksum starmap in
   printf "%d\n" checksum
+
+let part2 file =
+  let starmap = Starmap.read_exn file in
+  let distance = Starmap.distance starmap "YOU" "SAN" in
+  printf "%d\n" distance
