5ad596ecec5e4e65349e40b92ba0b9a874d7662b
Jordy Dickinson <jordy.dickinson@icloud.com>
2019-12-09 12:29:01 -0500

    Update day 5 to use new Intcode functions

--- src/day5.ml
+++ src/day5.ml
@@ -1,23 +1,19 @@
 open Core
 
 let part1 file =
-  let mem = Intcode.load file in
-  let rec part1' state =
-    let open Intcode in
-    match state with
-    | Receiving resume_with -> part1' @@ resume_with 1
-    | Sending (i, resume) -> printf "%d\n" i; part1' @@ resume ()
-    | Halted -> ()
-  in
-  part1' (Intcode.exec mem)
+  Intcode.(
+    load file
+    |> exec
+    |> send_exn ~input:1
+    |> collect_exn
+    |> List.iter ~f:(printf "%d\n")
+  )
 
 let part2 file =
-  let mem = Intcode.load file in
-  let rec part2' state =
-    let open Intcode in
-    match state with
-    | Receiving resume_with -> part2' @@ resume_with 5
-    | Sending (i, resume) -> printf "%d\n" i; part2' @@ resume ()
-    | Halted -> ()
-  in
-  part2' (Intcode.exec mem)
+  Intcode.(
+    load file
+    |> exec
+    |> send_exn ~input:5
+    |> returns_exn
+    |> printf "%d\n"
+  )
