b0e0ff7a29d090f5ac5c04c5580e4ac327281ffb
Jordy Dickinson <jordy.dickinson@icloud.com>
2019-12-09 13:21:39 -0500

    Cleanup day 7 and 9

--- src/day7.ml
+++ src/day7.ml
@@ -1,29 +1,31 @@
 open Core
 
 let part1 file =
-  let mem = Intcode.load file in
+  let machine = Intcode.load file in
   let settings = Util.permutations [0;1;2;3;4] in
-  let try_setting mem setting =
+  let try_setting machine setting =
     let rec try_setting' setting signal =
     match setting with
     | [] -> signal
     | phase :: setting ->
       let signal =
-        Intcode.exec mem
-        |> Intcode.send_exn ~input:phase
-        |> Intcode.send_exn ~input:signal
-        |> Intcode.returns_exn
+        Intcode.(
+          exec machine
+          |> send_exn ~input:phase
+          |> send_exn ~input:signal
+          |> returns_exn
+        )
       in
       try_setting' setting signal
     in
     try_setting' setting 0
   in
-  Sequence.map settings ~f:(try_setting mem)
+  Sequence.map settings ~f:(try_setting machine)
   |> Sequence.fold ~init:0 ~f:max
   |> printf "%d\n"
 
 let part2 file =
-  let mem = Intcode.load file in
+  let machine = Intcode.load file in
   let settings = Util.permutations [5;6;7;8;9] in
   let try_setting mem setting =
     let amps =
@@ -39,7 +41,8 @@ let part2 file =
       | Some (amp, amps) ->
         let amp, signal =
           Intcode.(
-            send_exn amp ~input:signal
+            amp
+            |> send_exn ~input:signal
             |> recv_exn
           )
         in
@@ -49,6 +52,6 @@ let part2 file =
     in
     try_setting' 0 amps
   in
-  Sequence.map settings ~f:(try_setting mem)
+  Sequence.map settings ~f:(try_setting machine)
   |> Sequence.fold ~init:0 ~f:max
   |> printf "%d\n"
--- src/day9.ml
+++ src/day9.ml
@@ -1,21 +1,19 @@
 open Core
 
 let part1 file =
-  let machine = Intcode.load file in
-  let values = Intcode.(
-    exec machine
+  Intcode.(
+    load file
+    |> exec
     |> send_exn ~input:1
     |> collect_exn
   )
-  in
-  List.iter values ~f:(printf "%d\n")
+  |> List.iter ~f:(printf "%d\n")
 
 let part2 file =
-  let machine = Intcode.load file in
-  let values = Intcode.(
-    exec machine
+  Intcode.(
+    load file
+    |> exec
     |> send_exn ~input:2
     |> collect_exn
   )
-  in
-  List.iter values ~f:(printf "%d\n")
+  |> List.iter ~f:(printf "%d\n")
